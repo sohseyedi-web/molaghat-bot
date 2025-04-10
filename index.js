b61838041c849c9ae06d235243af940dede97c47
sohseyedi <sohseyedi@gmail.com>
2025-04-10 21:49:42 +0330

    chore: enhance bot functionality with session management and new command handlers

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const characters = require("./app/constant/characters");
-const { Telegraf } = require("telegraf");
+const { Telegraf, session } = require("telegraf");
 require("dotenv").config();
 
 const {
@@ -8,11 +8,15 @@ const {
   onWriteCharacters,
   onMainSelection,
   onHelpCommand,
+  onShowMainMenu,
+  onPreviousCharacters,
 } = require("./app/handlers/botHandlers");
 
 // Initialize bot
 const bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN);
 const ADMIN_ID = process.env.MY_TELEGRAM_ID;
+
+bot.use(session({ defaultSession: () => ({ page: 0 }) }));
 
 // Register handlers
 bot.start(handleStart);
@@ -20,7 +24,16 @@ bot.hears("شخصیت های دیگر", onShowMoreCharacters);
 bot.hears("/personas", onShowMoreCharacters);
 bot.command("help", onHelpCommand);
 bot.hears(/.*/, onMainSelection);
-bot.on("text", onWriteCharacters);
+bot.hears("بازگشت", onPreviousCharacters);
+bot.hears("منو اصلی", onShowMainMenu);
+bot.on("text", (ctx) => {
+  if (characters.includes(ctx.message.text)) {
+    ctx.reply(`تو انتخاب کردی: ${ctx.message.text} ✅\nحالا سوالت رو بپرس.`);
+  } else {
+    onMainSelection(ctx);
+    onWriteCharacters(ctx);
+  }
+});
 
 characters.forEach((name) => {
   bot.hears(name, (ctx) => {
