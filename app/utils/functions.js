b99cabf06cfb5f6d08c590fd226841c4fb7dfdd0
sohseyedi <sohseyedi@gmail.com>
2025-04-10 11:17:45 +0330

    feat:create chunk function to utilities

--- app/utils/functions.js
+++ app/utils/functions.js
@@ -1,4 +1,3 @@
-
 const farsiDigits = ["۰", "۱", "۲", "۳", "۴", "۵", "۶", "۷", "۸", "۹"];
 
 function toPersianNumbersWithComma(n) {
@@ -15,7 +14,16 @@ function toPersianNumbers(n) {
   return n.toString().replace(/\d/g, (x) => farsiDigits[parseInt(x)]);
 }
 
+function chunk(array, size) {
+  const result = [];
+  for (let i = 0; i < array.length; i += size) {
+    result.push(array.slice(i, i + size));
+  }
+  return result;
+}
+
 module.exports = {
   toPersianNumbersWithComma,
   toPersianNumbers,
+  chunk,
 };
