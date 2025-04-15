fa427b6ab8bb6ee3af724f5d8b05092cb2dc4ecd
sohseyedi <sohseyedi@gmail.com>
2025-04-15 21:52:55 +0330

    chore: add admin command handling and enhance user interaction options in bot

--- index.js
+++ index.js
@@ -1,21 +1,19 @@
-const characters = require("./app/constant/characters");
 const { Telegraf, session } = require("telegraf");
 require("dotenv").config();
 
 const {
   handleStart,
   onShowMoreCharacters,
-  onWriteCharacters,
   onMainSelection,
   onHelpCommand,
   onShowMainMenu,
   onPreviousCharacters,
   handleMessage,
+  handleAdminCommand,
 } = require("./app/handlers/botHandlers");
 
 // Initialize bot
 const bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN);
-const ADMIN_ID = process.env.MY_TELEGRAM_ID;
 
 bot.use(session({ defaultSession: () => ({ page: 0 }) }));
 
@@ -30,6 +28,9 @@ bot.hears("شخصیت‌ها", (ctx) => onMainSelection(ctx));
 bot.hears("پیشنهاد شخصیت", (ctx) => onMainSelection(ctx));
 bot.hears("خرید اشتراک", (ctx) => onMainSelection(ctx));
 bot.hears("راهنمای ربات", (ctx) => onMainSelection(ctx));
+bot.hears("ارسال پیام", (ctx) => onMainSelection(ctx));
+bot.hears("لغو ارسال", (ctx) => onMainSelection(ctx));
+bot.command("admin", handleAdminCommand);
 
 // Use character selection and message handling
 bot.on("text", handleMessage);
